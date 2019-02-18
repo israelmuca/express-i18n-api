02afd1f31031f3bb983a034dcd793295a3e3e1c9
Israel Muñoz <isra.munoz@gmail.com>
2019-02-18 15:28:02 -0600

    Changed the error messages for more specific ones, and added the validator that uses those error messages

---
+++ i18n/i18n.js
@@ -0,0 +1,15 @@
+export const availableLangs = ['es', 'en']
+
+export const messages = {
+    en: {
+        'emailRequiredField': "'email' is a required field.",
+        'emailIsEmail': "This is not a valid email address.",
+        'passwordRequiredField': "'password' is a required field.",
+        
+    },
+    es: {
+        'emailRequiredField': "'email' es un campo requerido.",
+        'emailIsEmail': "Este no es un email válido.",
+        'passwordRequiredField': "'password' es un campo requerido."
+    }
+}
---
+++ validator/auth.validator.js
@@ -0,0 +1,33 @@
+import { check } from 'express-validator/check'
+
+/*
+* Function to validate the user's input before passing it to the controller's functions
+* Receives the function to validate for
+* Validates the input using express-validator
+* Returns the actual validations in the req, to be used later in the chain of functions
+*/
+exports.validator = functionName => {
+
+    switch (functionName) {
+
+        case 'login': {
+            return [
+                check('email')
+                    .exists().withMessage('emailRequiredField')
+                    .isEmail().withMessage('emailIsEmail'),
+            
+                check('password')
+                    .exists().withMessage('passwordRequiredField')
+            ]
+        }
+
+        case 'forgotPassword': {
+            return [
+                check('email')
+                    .exists().withMessage('emailRequiredField')
+                    .isEmail().withMessage('emailIsEmail')
+            ]
+        }
+    }
+
+}
