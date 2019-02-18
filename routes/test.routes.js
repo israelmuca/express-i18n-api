13b03b3c5633e15c9b20e4209ab7d912cd86a931
Israel Muñoz <isra.munoz@gmail.com>
2019-02-18 15:04:59 -0600

    Added routes

---
+++ routes/test.routes.js
@@ -0,0 +1,11 @@
+// Routes =============================================================
+module.exports = router => {
+
+    // GET route to mock a login  endpoint
+    router.post("/api/login")
+
+    // GET route to mock a forgotten password endpoint
+    router.post("/api/forgot-password")
+
+
+}
--- server.js
+++ server.js
@@ -9,16 +9,22 @@ import bodyParser from 'body-parser'
 // =============================================================
 const router = express()
 
+// Add data parsing to express
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(bodyParser.json())
+
 // Get the user's locale, and set a default in case there's none
 router.use(createLocaleMiddleware({
     "priority": ["accept-language", "default"],
     "default": "en_US"
 }))
 
-// Add data parsing to express
-router.use(bodyParser.urlencoded({ extended: true }))
-router.use(bodyParser.json())
+// Start polyglot and set the language in the req with the phrases to be used
+router.use(startPolyglot)
 
+// Routes
+// =============================================================
+require("./routes/test.routes")(router)
 
 // Start the server!
 // =============================================================
