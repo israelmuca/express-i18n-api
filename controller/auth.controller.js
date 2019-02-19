7b35d4ade6e48277606db7606ae91dfd49c4411e
Israel Muñoz <isra.munoz@gmail.com>
2019-02-19 10:55:56 -0600

    Added the controller to respond back to the user when everything went ok

---
+++ controller/auth.controller.js
@@ -0,0 +1,32 @@
+exports.login = (req, res) => {
+
+    // If no validation errors, get the req.body objects that were validated and are needed
+    const { email, password } = req.body
+
+    /*
+    * Here we would probably call the DB to confirm the user exists
+    * Then validate if they're authorized to login
+    * Then confirm their password
+    * Create a JWT or a cookie
+    * And finally send it back if all's good
+    */
+
+    // Since all the validations passed, we send the loginSuccessful message, which would normally include a JWT or some other form of authorization
+    return res.status(200).send({ auth: true, message: req.polyglot.t('loginSuccessful'), token: null })
+}
+
+exports.forgotPassword = (req, res) => {
+    // If no validation errors, get the req.body objects that were validated and are needed
+    const { email } = req.body
+
+    /*
+    * Here we would probably call the DB to confirm the user exists
+    * Then validate if they're authorized to login
+    * Create a JWT or a cookie
+    * Send an email to that address with the URL to login directly to change their password
+    * And finally let the user know their email is waiting for them at their inbox
+    */
+
+    // Since all the validations passed, we send the emailSent message
+    return res.status(200).send({ auth: true, message: req.polyglot.t('emailSent') })
+}
