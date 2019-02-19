dd492996680159da7b56de4d2b74442980e86b7f
Israel Muñoz <isra.munoz@gmail.com>
2019-02-19 10:34:25 -0600

    Added the .js to process the errors

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -2,14 +2,23 @@ export const availableLangs = ['es', 'en']
 
 export const messages = {
     en: {
+        // Error messages
         'emailRequiredField': "'email' is a required field.",
         'emailIsEmail': "This is not a valid email address.",
         'passwordRequiredField': "'password' is a required field.",
         
+        // Success messages
+        'loginSuccessful': "You've successfully logged in.",
+        'emailSent': "Your password recovery email was sent."
     },
     es: {
+        // Error messages
         'emailRequiredField': "'email' es un campo requerido.",
         'emailIsEmail': "Este no es un email válido.",
-        'passwordRequiredField': "'password' es un campo requerido."
+        'passwordRequiredField': "'password' es un campo requerido.",
+        
+        // Success messages
+        'loginSuccessful': "Has iniciado sesión exitosamente.",
+        'emailSent': "Tu correo de recuperación de contraseña ha sido enviado."
     }
 }
